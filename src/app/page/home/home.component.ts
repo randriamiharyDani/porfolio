cc11e3c5f3572db030ca684bc4400d1b7bf5b32a
randriamiharyDani <randriamiharySteev@gmail.com>
2025-01-07 11:37:00 +0300

    feature : annimation slide

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
 import { ProfileComponent } from "../profile/profile.component";
 import { CompetenceComponent } from "../competence/competence.component";
+import { SlideComponent } from "../slide/slide.component";
 
 @Component({
   selector: 'app-home',
-  imports: [NavBarComponent, ProfileComponent, CompetenceComponent],
+  imports: [NavBarComponent, ProfileComponent, CompetenceComponent, SlideComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
