09d9e10460cacc1ac6fa660e5555cae12f4931db
randriamiharyDani <randriamiharySteev@gmail.com>
2025-01-07 14:58:32 +0300

    ui: component and footer

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -3,10 +3,11 @@ import { NavBarComponent } from '../nav-bar/nav-bar.component';
 import { ProfileComponent } from "../profile/profile.component";
 import { CompetenceComponent } from "../competence/competence.component";
 import { SlideComponent } from "../slide/slide.component";
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-home',
-  imports: [NavBarComponent, ProfileComponent, CompetenceComponent, SlideComponent],
+  imports: [NavBarComponent, ProfileComponent, CompetenceComponent, SlideComponent,FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
