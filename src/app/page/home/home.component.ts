e4fad48ab139555fd05f2dcc60842b60acdf8b1b
randriamiharyDani <randriamiharySteev@gmail.com>
2025-01-17 15:18:21 +0300

    ui : gsap

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
 import { ProfileComponent } from "../profile/profile.component";
 import { CompetenceComponent } from "../competence/competence.component";
 import { SlideComponent } from "../slide/slide.component";
 import { FooterComponent } from '../footer/footer.component';
+import gsap from "gsap";
 
 @Component({
   selector: 'app-home',
@@ -11,6 +12,18 @@ import { FooterComponent } from '../footer/footer.component';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
   imagePath = '/assets/profile.jpg';
+
+   ngOnInit() {
+    // Animation GSAP
+    gsap.to(".img-fluid", {
+      duration: 2,
+      transition:3,
+      x: 250,
+      // y: 30,
+      opacity: 0.9,
+      ease: "power2.in"
+    });
+  }
 }
