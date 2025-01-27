ba3692bdfadb7353688b90b764d9048ce0ab95f0
randriamiharyDani <randriamiharySteev@gmail.com>
2025-01-27 09:45:14 +0300

    ui : enlever text dans le profil

--- src/app/page/profile/profile.component.ts
+++ src/app/page/profile/profile.component.ts
@@ -10,8 +10,7 @@ export class ProfileComponent {
 
   profileParagraphe = ` Je suis un développeur web junior passionné par la technologie et le développement logiciel.
    Spécialisé en Angular et Laravel, j'ai acquis des compétences solides dans la création d'applications web modernes et dynamiques. Bien que je sois au début de ma carrière, j'ai une grande volonté d'apprendre et de contribuer à des projets concrets.
-   Mon objectif est de perfectionner mes compétences techniques tout en apportant une valeur ajoutée aux équipes avec lesquelles je collabore.
-    Voici quelques informations sur moi :` ;
+   Mon objectif est de perfectionner mes compétences techniques tout en apportant une valeur ajoutée aux équipes avec lesquelles je collabore.` ;
 
 
 }
